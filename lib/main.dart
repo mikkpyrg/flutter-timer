19efbe81b54857706f184a22cb9319b1a8407298
mikkpyrg <mikk.pyrg@gmail.com>
2019-08-15 18:59:22 +0300

    timer and list view starting to take shape

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:developer' as developer;
+import 'dart:async';
+import 'dart:developer' as con;
 // flutter emulators --launch nexus
 // flutter run
 // r for hot reload
@@ -17,17 +18,95 @@ class MyApp extends StatelessWidget {
     	theme: ThemeData(
         primaryColor: Colors.white,
       ),
-      home: Timer(),
+      home: TimerWidget(),
     );
   }
 }
 
-class Timer extends StatefulWidget {
+class Track {
+  String name;
+  Duration track;
+
+  Track(this.name, this.track);
+}
+
+class TimerWidget extends StatefulWidget {
   @override
   TimerState createState() => TimerState();
 }
 
-class TimerState extends State<Timer> {
+class TimerState extends State<TimerWidget> {
+  final List<Track> _tracks = [new Track('mari', new Duration(seconds: 15)), 
+    new Track('kari', new Duration(seconds: 16)),
+    new Track('tari', new Duration(seconds: 17)),
+    new Track('pari', new Duration(seconds: 18)),
+  ];
+  int _selectedTrackIndex = 0;
+  Duration _remaining;
+  Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _remaining = new Duration(seconds: _tracks[_selectedTrackIndex].track.inSeconds);
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void _setTime(Timer timer) {
+    Duration _timeLeft = _remaining - Duration(milliseconds: 10);
+    if (_timeLeft.isNegative) {
+      setState(() {
+        _remaining = Duration(milliseconds: 0);
+      });
+      _timer.cancel();
+      _timeExceeded();
+    } else {
+      setState(() {
+        _remaining = _timeLeft;
+      });
+    }
+  }
+
+  void _timeExceeded() {
+    con.log('beeeeep');
+  }
+
+  void _resetTimer() {
+    if (_timer != null && _timer.isActive) {
+      _timer.cancel();
+    }
+    Track _selectedTrack = _tracks[0];
+    setState(() {
+      _selectedTrackIndex = 0;
+      _remaining = new Duration(seconds: _selectedTrack.track.inSeconds);
+    });
+  }
+
+  void _passTimer() {
+    if (_timer == null || !_timer.isActive) {
+      _timer = new Timer.periodic(const Duration(milliseconds: 10), _setTime);
+    }
+    else {
+      _selectTrack(_selectedTrackIndex < (_tracks.length - 1) ? _selectedTrackIndex + 1 : 0);
+    }
+  }
+
+  void _selectTrack(int index) {
+
+    Track _selectedTrack = _tracks[index];
+    setState(() {
+      _selectedTrackIndex = index;
+      _remaining = new Duration(seconds: _selectedTrack.track.inSeconds);
+    });
+    if (_timer == null || !_timer.isActive) {
+      _timer = new Timer.periodic(const Duration(milliseconds: 10), _setTime);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,13 +115,65 @@ class TimerState extends State<Timer> {
 	      title: Text('Timer'),
 	    ),
 	    body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Text('ggaa'),
-          Text('wud')
-
+          _mainButton(),
+          _trackList()
         ]
       )
 	  );
+  }
+
+  String get timeString {
+    return '${_remaining.inMinutes}:${(_remaining.inSeconds % 60).toString().padLeft(2, '0')}.${((_remaining.inMilliseconds % 1000) / 10).toStringAsFixed(0).padLeft(2, '0')}';
+  }
+
+  Widget _mainButton() {
+    return Expanded (
+      child: Padding(
+        padding: EdgeInsets.all(5),
+        child: GestureDetector( 
+          onTap: () => _passTimer(),
+          onLongPress: () => _resetTimer(),
+          child: Container (
+            color: Colors.green,
+            child: Center( 
+              child:Column (
+                children: [
+                  Text(timeString)
+                ]
+              )
+            ),
+          ),
+        ),
+      )
+    );
+  }
+
+  Widget _trackList() {
+    return Container (
+    width: double.infinity,
+    height: 170,
+    child: ListView.builder(
+      padding: EdgeInsets.all(5),
+      itemCount: _tracks.length,
+      itemBuilder: (context, i) {
+        Track _trackItem = _tracks[i];
+        return Container ( 
+          color: _selectedTrackIndex == i ? Colors.lightGreen : Colors.lightBlue,
+          child: ListTile(
+            title: Text(
+              _trackItem.name,
+            ),
+            leading: Icon(
+              _selectedTrackIndex == i ? Icons.timelapse : Icons.timer_off,
+              color: _selectedTrackIndex == i ? Colors.green : Colors.yellow,
+            ),
+            onTap: () => _selectTrack(i),
+          ),
+        );
+      })
+    );
+
+
   }
 }
